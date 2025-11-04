60dde6d0d8ac64cdf0100c5142657693fb5c47e3
Nancy Adelice Umutoniwase <nancytoniw@gmail.com>
2025-11-04 19:24:30 +0200

    Migrations applied

--- models.py
+++ models.py
@@ -3,9 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 db=SQLAlchemy()
 
 class Student(db.Model):
+	__tablename__="students"
 
 	id=db.Column(db.Integer,primary_key=True)
 	name=db.Column(db.String(100))
 	age=db.Column(db.Integer)
 	email=db.Column(db.String(120),unique=True)
 	gender=db.Column(db.String)
+
+class Course (db.Model):
+	__tablename__="courses"
+
+	id=db.Column(db.Integer,primary_key=True)
+	title=db.Column(db.String(100), nullable=False)
+	description=db.Column(db.Text)
+	instructor=db.Column(db.String(100))
+	credits=db.Column(db.Integer)
