99c79ee15accee426ea10988a2d78f165fde0579
Nancy Adelice Umutoniwase <nancytoniw@gmail.com>
2025-11-06 19:25:06 +0200

    Convert to JSON

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import db, Student 
@@ -12,10 +12,54 @@ db.init_app(app)
 
 migrate=Migrate(app,db)
 
+# @app.route('/')
+# def home():
+# 	students=Student.query.all()
+# 	response_body='<h1>Students Names</h1>'
+# 	for s in students:
+# 		response_body+=f'<p>{s.name}</p>'#response_body=response_body+f'<p>{s.name}</p> x+=1=>x=x+1
+# 	response=make_response(response_body,200)
+# 	return response
+
+# @app.route('/students/<int:id>')	
+# def student_by_id(id):
+# 	student=Student.query.get(id)
+# 	if student:
+# 		response_body=f'<p>{student.name}</p>'
+# 		status=200
+# 	else:
+# 		response_body=f'<p>Student {id} not found</p>'
+# 		status=404
+# 	response=make_response(response_body,status)
+# 	return response
 @app.route('/')
 def home():
-	return "Welcome to the Student App"
-	
+	students=Student.query.all()
+	students_data=[{
+		"id":student.id,
+		"name":student.name,
+		"email":student.email,
+		"age":student.age,
+		"gender":student.gender
+	} for student in students
+	]
+	return jsonify (students_data),200
+
+@app.route('/students/<int:id>')
+def student_json(id):
+	student=Student.query.get(id)
+	if not student:
+		return jsonify({"error":f"Student {id} not found"}),404
+	student_data={
+		"id":student.id,
+		"name":student.name,
+		"email":student.email,
+		"age":student.age,
+		"gender":student.gender
+	}
+	return jsonify(student_data),200
+
+
 @app.cli.command("seed")
 def seed():
 	from seeds import run_seeds
